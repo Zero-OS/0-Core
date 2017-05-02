20405c835ff04b5aa343f410869c35aa85bcc6aa
Muhamad Azmy <muhamad.azmy@codescalers.com>
2017-05-02 11:32:36 +0200

    Table container list

--- corectl/container.go
+++ corectl/container.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/codegangsta/cli"
 	"github.com/g8os/core0/base/pm/core"
+	"github.com/olekukonko/tablewriter"
+	"os"
+	"strings"
 )
+
+type containerData struct {
+	Container struct {
+		Arguments struct {
+			Root     string   `json:"root"`
+			Hostname string   `json:"hostname"`
+			Tags     []string `json:"tags"`
+		} `json:"arguments"`
+		PID  int    `json:"pid"`
+		Root string `json:"root"`
+	} `json:"container"`
+}
 
 func containers(t Transport, c *cli.Context) {
 	var tags []string
@@ -27,5 +43,23 @@ func containers(t Transport, c *cli.Context) {
 	}
 
 	response.ValidateResultOrExit()
-	response.PrintYaml()
+	var containers map[string]containerData
+	if err := json.Unmarshal([]byte(response.Data), &containers); err != nil {
+		log.Fatal(err)
+	}
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetBorders(tablewriter.Border{})
+	table.SetAlignment(tablewriter.ALIGN_LEFT)
+	table.SetHeader([]string{"ID", "FLIST", "HOSTNAME", "TAGS"})
+	for id, container := range containers {
+		table.Append([]string{
+			id,
+			container.Container.Arguments.Root,
+			container.Container.Arguments.Hostname,
+			strings.Join(container.Container.Arguments.Tags, ", "),
+		})
+	}
+
+	table.Render()
 }
