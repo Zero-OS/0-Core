7d6e6e5bd280cc3c87e7c8409767dfa4ed0d1e9b
Muhamad Azmy <muhamad.azmy@codescalers.com>
2017-07-13 08:53:57 +0200

    Fix bad merge

--- client/py-client/zeroos/core0/client/client.py
+++ client/py-client/zeroos/core0/client/client.py
@@ -956,7 +956,7 @@ class ContainerManager:
     def __init__(self, client):
         self._client = client
 
-    def create(self, root_url, mount=None, host_network=False, nics=DefaultNetworking, port=None, hostname=None, privileged=True, storage=None, name=None, tags=None):
+    def create(self, root_url, mount=None, host_network=False, nics=DefaultNetworking, port=None, hostname=None, privileged=False, storage=None, name=None, tags=None):
         """
         Creater a new container with the given root flist, mount points and
         zerotier id, and connected to the given bridges
