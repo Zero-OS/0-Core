fea2ba689abb70b19ab4be724d11b5db74969072
Muhamad Azmy <azmy@conictus.com>
2018-03-07 11:59:59 +0200

    Add missing file

---
+++ core0/transport/redis.go
@@ -0,0 +1,137 @@
+package transport
+
+import (
+	"strings"
+	"time"
+
+	"github.com/garyburd/redigo/redis"
+	"github.com/tidwall/redcon"
+	"github.com/zero-os/0-core/core0/assets"
+	"github.com/zero-os/0-core/core0/options"
+)
+
+func newPool() *redis.Pool {
+	return &redis.Pool{
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("unix", "/tmp/redis.sock")
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
+		MaxActive: 20,
+		Wait:      true,
+	}
+}
+
+type redisProxy struct {
+	pool       *redis.Pool
+	authMethod func(string) bool
+}
+
+//liste to core0 port and
+func Listen() error {
+	authMethod := func(_ string) bool {
+		return true
+	}
+
+	if orgs, ok := options.Options.Kernel.Get("organization"); ok {
+		org := orgs[len(orgs)-1]
+		var err error
+		authMethod, err = AuthMethod(org, string(assets.MustAsset("text/itsyouonline.pub")))
+		if err != nil {
+			return err
+		}
+	}
+
+	p := redisProxy{
+		pool:       newPool(),
+		authMethod: authMethod,
+	}
+
+	tlsConfig, err := generateCRT()
+	if err != nil {
+		return err
+	}
+
+	return redcon.ListenAndServeTLS(
+		":6379",
+		p.handler,
+		p.accept,
+		p.closed,
+		tlsConfig,
+	)
+}
+
+func (r *redisProxy) auth(conn redcon.Conn, cmd redcon.Command) {
+	if len(cmd.Args) != 2 {
+		conn.WriteError("invalid number of arguments")
+		return
+	}
+
+	password := string(cmd.Args[1])
+
+	if r.authMethod(password) {
+		conn.SetContext(true)
+		conn.WriteString("OK")
+	} else {
+		conn.WriteError("invalid jwt")
+	}
+}
+
+func (r *redisProxy) proxy(conn redcon.Conn, cmd redcon.Command) {
+	// is authorized ?
+	if ctx := conn.Context(); ctx == nil {
+		//ctx was not set, hence he either didn't call auth or not authorized
+		conn.WriteError("permission denied, please call AUTH first with a valid JWT")
+		return
+	}
+
+	//proxy to underlying redis
+	local := r.pool.Get()
+	defer local.Close()
+
+	args := make([]interface{}, 0, len(cmd.Args)-1)
+
+	for _, arg := range cmd.Args[1:] {
+		args = append(args, arg)
+	}
+
+	result, err := local.Do(string(cmd.Args[0]), args...)
+
+	if err != nil {
+		conn.WriteError(err.Error())
+	} else if result, err := redis.Int64(result, err); err != nil {
+		conn.WriteInt64(result)
+	} else if result, err := redis.String(result, err); err != nil {
+		if result == "OK" || result == "PONG" {
+			conn.WriteString(result)
+		} else {
+			conn.WriteBulkString(result)
+		}
+	} else if result, err := redis.Strings(result, err); err != nil {
+		conn.WriteArray(len(result))
+		for _, r := range result {
+			conn.WriteBulkString(r)
+		}
+	} else {
+		conn.WriteError("unhandled return type")
+	}
+}
+
+func (r *redisProxy) handler(conn redcon.Conn, cmd redcon.Command) {
+	switch strings.ToLower(string(cmd.Args[0])) {
+	case "auth":
+		r.auth(conn, cmd)
+	default:
+		r.proxy(conn, cmd)
+	}
+}
+
+func (r *redisProxy) accept(conn redcon.Conn) bool {
+	return true
+}
+
+func (r *redisProxy) closed(conn redcon.Conn, err error) {
+
+}
