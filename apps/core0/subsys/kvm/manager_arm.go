92991c92a59103d43e6e4dcd750de89eaa0b200b
Maxime Daniel <root@maxux.net>
2018-03-22 11:28:52 +0100

    Avoid libvirt when compiling kvm on ARM plateform

---
+++ apps/core0/subsys/kvm/manager_arm.go
@@ -0,0 +1,19 @@
+// +build arm
+
+package kvm
+
+import (
+	"github.com/op/go-logging"
+	"github.com/zero-os/0-core/apps/core0/screen"
+	"github.com/zero-os/0-core/apps/core0/subsys/containers"
+)
+
+var (
+	log = logging.MustGetLogger("kvm")
+)
+
+func KVMSubsystem(conmgr containers.ContainerManager, cell *screen.RowCell) error {
+	log.Errorf("kvm disabled on arm")
+	return nil
+}
+
