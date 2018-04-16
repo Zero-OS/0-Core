68db8a3733fd12d9a2a1933fbc99bb0759100b9c
Ahmed T. Youssef <xmonader@gmail.com>
2018-04-16 12:40:38 +0200

    Update code to use buitlin and reduce complexity by using uint

--- apps/core0/builtin/hdparm.go
+++ apps/core0/builtin/hdparm.go
@@ -3,20 +3,9 @@ package builtin
 import (
 	"encoding/json"
 	"fmt"
-	"os"
 	"github.com/zero-os/0-core/base/pm"
+	"github.com/zero-os/0-core/base/utils"
 )
-
-func exists(path string) (bool, error) {
-    _, err := os.Stat(path)
-    if err == nil {
-		 return true, nil
-	 }
-    if os.IsNotExist(err) {
-		 return false, nil 
-	}
-    return true, nil 
-}
 
 func init() {
 	pm.RegisterBuiltIn("core.spindown_disk", hdparmSpindownDisk)
@@ -25,27 +14,25 @@ func init() {
 func hdparmSpindownDisk(cmd *pm.Command) (interface{}, error) {
 	var args struct {
 		DiskPath string `json:"disk_path"`
-		Spindown int	`json:"spindown"`
+		Spindown uint	`json:"spindown"`
 	}
-	cmdbin := "hdparm"
 	if err := json.Unmarshal(*cmd.Arguments, &args); err != nil {
 		return nil, err
 	}
 	// assert disk exists
-	_, err := exists(args.DiskPath)
-	if err!= nil {
-		return nil, pm.BadRequestError(fmt.Errorf("Disk doesn't exist: %s", args.DiskPath))
-	
+	if !utils.Exists(args.DiskPath){
+		return nil, pm.BadRequestError(fmt.Errorf("disk doesn't exist: %s", args.DiskPath))
+
 	}
-	if !(args.Spindown>0 && args.Spindown<241){
-		return nil, pm.BadRequestError(fmt.Errorf("Spindown %d out of range 0 - 240", args.Spindown))
+	if !(args.Spindown<241){
+		return nil, pm.BadRequestError(fmt.Errorf("spindown %d out of range 1 - 240", args.Spindown))
 	
 	} 
-
-	_, err = pm.System(cmdbin, "-S", fmt.Sprintf("%d", args.Spindown), args.DiskPath)
+	_, err := pm.System("hdparm", "-S", fmt.Sprintf("%d", args.Spindown), args.DiskPath)
 
 	if err != nil {
 		return nil, err
 	}
+
 	return nil, nil
 }
