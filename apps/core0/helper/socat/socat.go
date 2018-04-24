47ae9f08b3e1beda5bb3ab6be505548e97971311
Muhamad Azmy <muhamada@greenitglobe.com>
2018-04-24 14:01:14 +0200

    Port forwards with dnat

--- apps/core0/helper/socat/socat.go
+++ apps/core0/helper/socat/socat.go
@@ -4,79 +4,145 @@ import (
 	"fmt"
 	"strings"
 	"sync"
-	"syscall"
 
-	"encoding/json"
+	"github.com/zero-os/0-core/base/nft"
 
 	"github.com/op/go-logging"
-	"github.com/zero-os/0-core/base/builtin"
-	"github.com/zero-os/0-core/base/pm"
 )
 
 var (
 	log  = logging.MustGetLogger("socat")
 	lock sync.Mutex
+
+	rules = map[int]rule{}
 )
 
-func SetPortForward(id string, ip string, host int, dest int) error {
+type rule struct {
+	ns   string
+	port int
+	ip   string
+}
+
+func (r rule) Rule(host int) string {
+	return fmt.Sprintf("tcp dport %d dnat to %s:%d", host, r.ip, r.port)
+}
+
+//SetPortForward create a single port forward from host, to dest in this namespace
+//The namespace is used to group port forward rules so they all can get terminated
+//with one call later.
+func SetPortForward(namespace string, ip string, host int, dest int) error {
 	lock.Lock()
 	defer lock.Unlock()
 
-	var ports []*builtin.Port
-	job, err := pm.Run(&pm.Command{
-		Command: "info.port",
-	})
-	result := job.Wait()
-	if err := json.Unmarshal([]byte(result.Data), &ports); err != nil {
+	//NOTE: this will only check if the port is used for port forwarding
+	//if a port on the host is using this port it will get masked out
+	if _, exists := rules[host]; exists {
+		return fmt.Errorf("port already in use")
+	}
+
+	r := rule{
+		ns:   forwardId(namespace, host, dest),
+		port: dest,
+		ip:   ip,
+	}
+
+	set := nft.Nft{
+		"nat": nft.Table{
+			Family: nft.FamilyIP,
+			Chains: nft.Chains{
+				"pre": nft.Chain{
+					Rules: []nft.Rule{
+						{Body: r.Rule(host)},
+					},
+				},
+			},
+		},
+	}
+
+	if err := nft.Apply(set); err != nil {
 		return err
 	}
 
-	for _, port := range ports {
-		if port.Network != "unix" && int(port.Port) == host {
-			return fmt.Errorf("Can't forward from port %d, host is already listening on it", host)
-		}
+	rules[host] = r
+	return nil
+}
+
+func forwardId(namespace string, host int, dest int) string {
+	return fmt.Sprintf("socat-%v-%v-%v", namespace, host, dest)
+}
+
+//RemovePortForward removes a single port forward
+func RemovePortForward(namespace string, host int, dest int) error {
+	lock.Lock()
+	defer lock.Unlock()
+	rule, ok := rules[host]
+	if !ok {
+		return fmt.Errorf("no port forwrard from host port: %d", host)
 	}
 
-	//nft add rule nat prerouting iif eth0 tcp dport { 80, 443 } dnat 192.168.1.120
-	cmd := &pm.Command{
-		ID:      forwardId(id, host, dest),
-		Command: pm.CommandSystem,
-		Flags: pm.JobFlags{
-			NoOutput: true,
-		},
-		Arguments: pm.MustArguments(
-			pm.SystemCommandArguments{
-				Name: "socat",
-				Args: []string{
-					fmt.Sprintf("tcp-listen:%d,reuseaddr,fork", host),
-					fmt.Sprintf("tcp-connect:%s:%d", ip, dest),
+	if rule.ns != forwardId(namespace, host, dest) {
+		return fmt.Errorf("permission denied")
+	}
+
+	set := nft.Nft{
+		"nat": nft.Table{
+			Family: nft.FamilyIP,
+			Chains: nft.Chains{
+				"pre": nft.Chain{
+					Rules: []nft.Rule{
+						{Body: rule.Rule(host)},
+					},
 				},
 			},
-		),
-	}
-	onExit := &pm.ExitHook{
-		Action: func(s bool) {
-			log.Infof("Port forward %d:%d with id %s exited", host, dest, id)
 		},
 	}
 
-	_, err = pm.Run(cmd, onExit)
-	return err
+	return nft.Apply(set)
 }
 
-func forwardId(id string, host int, dest int) string {
-	return fmt.Sprintf("socat-%v-%v-%v", id, host, dest)
-}
+//RemoveAll remove all port forwrards that were created in this namespace.
+func RemoveAll(namespace string) error {
+	lock.Lock()
+	defer lock.Unlock()
 
-func RemovePortForward(id string, host int, dest int) error {
-	return pm.Kill(forwardId(id, host, dest))
-}
+	var todelete []nft.Rule
+	var hostPorts []int
 
-func RemoveAll(id string) {
-	for key, job := range pm.Jobs() {
-		if strings.HasPrefix(key, fmt.Sprintf("socat-%s", id)) {
-			log.Debugf("stopping socat process: %s", key)
-			job.Signal(syscall.SIGTERM)
+	for host, r := range rules {
+		if !strings.HasPrefix(r.ns, fmt.Sprintf("socat-%s", namespace)) {
+			continue
 		}
+
+		todelete = append(todelete, nft.Rule{
+			Body: r.Rule(host),
+		})
+
+		hostPorts = append(hostPorts, host)
 	}
+
+	if len(todelete) == 0 {
+		return nil
+	}
+
+	set := nft.Nft{
+		"nat": nft.Table{
+			Family: nft.FamilyIP,
+			Chains: nft.Chains{
+				"pre": nft.Chain{
+					Rules: todelete,
+				},
+			},
+		},
+	}
+
+	if err := nft.DropRules(set); err != nil {
+		log.Errorf("failed to delete ruleset: %s", err)
+		return err
+	}
+
+	for _, host := range hostPorts {
+		delete(rules, host)
+	}
+
+	return nil
 }
