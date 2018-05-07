a97ac2087f1a59701373663e8f6ef7df8cf4b803
Ahmed El-Sayed <ahmed.m.elsayed93@gmail.com>
2018-05-07 14:59:55 +0200

    fix

--- tests/testsuite/b_extended/tests_03_networking.py
+++ tests/testsuite/b_extended/tests_03_networking.py
@@ -20,18 +20,10 @@ class ExtendedNetworking(BaseTest):
 
         **Test Scenario:**
         #. Get NetworkId (N1) using zerotier API
-        #. G8os client join zerotier network (N1)
         #. Create 2 containers c1, c2 and make them join (N1)
-        #. Get g8os and containers zerotier ip addresses
-        #. Container c1 ping g8os client, should succeed
+        #. Get containers zerotier ip addresses
         #. Container c1 ping Container c2, should succeed
-        #. Container c2 ping g8os client, should succeed
         #. Container c2 ping Container c1, should succeed
-        #. G8os client ping Container c1, should succeed
-        #. G8os client ping Container c2, should succeed
-        #. G8os client leave zerotier network (N1), should succeed
-        #. G8os client ping Container c1, should fail
-        #. G8os client ping Container c2, should fail
         #. Terminate containers c1, c2
         """
         self.lg('{} STARTED'.format(self._testID))
@@ -40,9 +32,6 @@ class ExtendedNetworking(BaseTest):
         networkId = self.create_zerotier_network()
 
         try:
-            self.lg('Join zerotier network (N1)')
-            self.client.zerotier.join(networkId)
-
             self.lg('Create 2 containers c1, c2 and make them join (N1) && create there clients')
             nic = [{'type': 'default'}, {'type': 'zerotier', 'id': networkId}]
             cid_1 = self.create_container(root_url=self.root_url, storage=self.storage, nics=nic)
@@ -50,56 +39,24 @@ class ExtendedNetworking(BaseTest):
             c1_client = self.client.container.client(cid_1)
             c2_client = self.client.container.client(cid_2)
 
-            time.sleep(30)
+            time.sleep(40)
 
-            self.lg('Get g8os and containers zerotier ip addresses')
-            g8_ip = self.get_g8os_zt_ip(networkId)
+            self.lg('Get containers zerotier ip addresses')
             c1_ip = self.get_contanier_zt_ip(c1_client, networkId)
             c2_ip = self.get_contanier_zt_ip(c2_client, networkId)
-
-            self.assertTrue(all([g8_ip, c1_ip, c2_ip]))
-
-            time.sleep(20)
+            
+            time.sleep(30)
 
             self.lg('set client time to 100 sec')
             self.client.timeout = 100
-
-            self.lg('Container c1 ping g8os client (ip : {}), should succeed'.format(g8_ip))
-            r = c1_client.bash('ping -w5 {}'.format(g8_ip)).get()
-            self.assertEqual(r.state, 'SUCCESS', r.stdout)
 
             self.lg('Container c1 ping Container c2 (ip : {}), should succeed'.format(c2_ip))
             r = c1_client.bash('ping -w5 {}'.format(c2_ip)).get()
             self.assertEqual(r.state, 'SUCCESS', r.stdout)
 
-            self.lg('Container c2 ping g8os client (ip : {}), should succeed'.format(g8_ip))
-            r = c2_client.bash('ping -w5 {}'.format(g8_ip)).get()
-            self.assertEqual(r.state, 'SUCCESS', r.stdout)
-
             self.lg('Container c2 ping Container c1 (ip : {}), should succeed'.format(c1_ip))
             r = c2_client.bash('ping -w5 {}'.format(c1_ip)).get()
             self.assertEqual(r.state, 'SUCCESS', r.stdout)
-
-            self.lg('G8os client ping Container c1 (ip : {}), should succeed'.format(c1_ip))
-            r = self.client.bash('ping -w5 {}'.format(c1_ip)).get()
-            self.assertEqual(r.state, 'SUCCESS', r.stdout)
-
-            self.lg('G8os client ping Container c2 (ip : {}), should succeed'.format(c2_ip))
-            r = self.client.bash('ping -w5 {}'.format(c2_ip)).get()
-            self.assertEqual(r.state, 'SUCCESS', r.stdout)
-
-            self.lg('G8os client leave zerotier network (N1), should succeed')
-            self.client.zerotier.leave(networkId)
-
-            time.sleep(5)
-
-            self.lg('G8os client ping Container c1 (ip : {}), should fail'.format(c1_ip))
-            r = self.client.bash('ping -w10 {}'.format(c1_ip)).get()
-            self.assertEqual(r.state, 'ERROR', r.stdout)
-
-            self.lg('G8os client ping Container c2 (ip : {}), should fail'.format(c2_ip))
-            r = self.client.bash('ping -w10 {}'.format(c2_ip)).get()
-            self.assertEqual(r.state, 'ERROR', r.stdout)
 
         finally:
             self.client.container.terminate(cid_1)
