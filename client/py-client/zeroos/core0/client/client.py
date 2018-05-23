4329e2174814da950ef577015bb61adcea472759
Ahmed T. Youssef <xmonader@gmail.com>
2018-05-23 13:21:21 +0200

    Add rtinfomgr

--- client/py-client/zeroos/core0/client/client.py
+++ client/py-client/zeroos/core0/client/client.py
@@ -347,6 +347,15 @@ class InfoManager:
         """
         return self._client.json('info.version', {})
 
+    def dmi(self, *types):
+        """
+        Get dmi output
+        :return: dict
+        """
+
+        return self._client.json('info.dmi', {'types': types})
+
+
 
 class JobManager:
     _job_chk = typchk.Checker({
@@ -2709,6 +2718,62 @@ class AggregatorManager:
 
         return self._client.json('aggregator.query', args)
 
+class RTInfoManager:
+    _rtinfo_start_params_chk = typchk.Checker({
+        'host': str,
+        'port': int,
+        'disks': [str]
+    })
+
+    _rtinfo_stop_params_chk = typchk.Checker({
+        'host': str,
+        'port': int,
+    })
+
+    def __init__(self, client):
+        self._client = client
+
+    def start(self, host="localhost", port=8999, disks=None):
+        """
+        Start rtinfo-client
+        :param host: str rtinfod host address
+        :param port: int rtinfod client port
+        :param disks: list of prefixes of wathable disks (e.g ["sd"])
+
+        """
+        disks = [] if disks is None else disks
+
+        args = {
+            "host": host,
+            "port": port,
+            "disks": disks
+        }
+        self._rtinfo_start_params_chk.check(args)
+
+        return self._client.json("rtinfo.start", args)
+
+    def stop(self, host, port):
+        """
+        Stop rtinfo-client
+        :param host: str rtinfod host address
+        :param port: int rtinfod client port
+        """
+
+        args = {
+            "host": host,
+            "port": port,
+        }
+        self._rtinfo_stop_params_chk.check(args)
+
+        return self._client.json("rtinfo.stop", args)
+
+    def list(self):
+        """
+        List running rtinfo clients
+        """
+        return self._client.json("rtinfo.list", {})
+
+
 
 class Client(BaseClient):
     _raw_chk = typchk.Checker({
@@ -2745,6 +2810,7 @@ class Client(BaseClient):
         self._nft = Nft(self)
         self._config = Config(self)
         self._aggregator = AggregatorManager(self)
+        self._rtinfo = RTInfoManager(self)
 
         if testConnectionAttempts:
             for _ in range(testConnectionAttempts):
@@ -2836,6 +2902,13 @@ class Client(BaseClient):
         """
         return self._aggregator
 
+    @property
+    def rtinfo(self):
+        """
+        RTInfo manager
+        """
+        return self._rtinfo
+
     def raw(self, command, arguments, queue=None, max_time=None, stream=False, tags=None, id=None):
         """
         Implements the low level command call, this needs to build the command structure
@@ -2876,3 +2949,4 @@ class Client(BaseClient):
 
     def response_for(self, id):
         return Response(self, id)
+
